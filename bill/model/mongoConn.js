ac797c0b473a9fead21476bf660d2fa6e522caa3
dung <dungvq123@gmail.com>
2021-10-20 07:43:37 +0700

    file main 1

--- bill/model/mongoConn.js
+++ bill/model/mongoConn.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const database = "CuoiKhoaFullStack52"
 function connectMongo() {
     const database = "BanSach";
-    mongoose.connect("mongodb+srv://dungvq123:dunglan123@cluster0.yyxl5.mongodb.net/bai8?retryWrites=true&w=majority", { useNewUrlParser: true });
+    mongoose.connect("mongodb+srv://dungvq123:dunglan123@cluster0.yyxl5.mongodb.net/CuoiKhoaFullStack52?retryWrites=true&w=majority", { useNewUrlParser: true });
 
     const db = mongoose.connection;
     db.on("error", console.error.bind(console, "loi roi"));
